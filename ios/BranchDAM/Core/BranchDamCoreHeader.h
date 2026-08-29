4df4793298cfc3f1686070b6eeb1e3323bc3c377
Björn Hansen <58235613+s3ntin3l8@users.noreply.github.com>
2026-08-29 18:28:17 +0000

    fix(ios): set SWIFT_OBJC_BRIDGING_HEADER for gomobile framework headers

--- ios/BranchDAM/Core/BranchDamCoreHeader.h
+++ ios/BranchDAM/Core/BranchDamCoreHeader.h
@@ -1,6 +1,8 @@
-// BranchDamCoreHeader.h - Definitions for branchdam-mobile Go core engine
+// BranchDamCoreHeader.h - Bridging Header for branchdam-mobile Go core engine
 #ifndef BranchDamCoreHeader_h
 #define BranchDamCoreHeader_h
+
+#import <Foundation/Foundation.h>
 
 #if __has_include(<BranchDamCore/BranchDamCore.h>)
 #import <BranchDamCore/BranchDamCore.h>
